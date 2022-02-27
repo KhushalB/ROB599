5adef25f5fa21d1a854e6d64fe6aebc66ca66f38
Khushal Brahmbhatt <khushalbr@gmail.com>
2022-02-26 21:52:19 -0800

    arm files - perception

---
+++ arm-files/Functions/perception.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python3
+# coding=utf8
+import sys
+sys.path.append('/home/khushal/ArmPi/')
+import cv2
+import time
+import Camera
+import threading
+from LABConfig import *
+from ArmIK.Transform import *
+from ArmIK.ArmMoveIK import *
+import HiwonderSDK.Board as Board
+from CameraCalibration.CalibrationConfig import *
+
+if sys.version_info.major == 2:
+    print('Please run this program with python3!')
+    sys.exit(0)
+
+# AK = ArmIK()
+
+
+class Perception:
+    def __init__(self):
+        self.range_rgb = {
+            'red': (0, 0, 255),
+            'blue': (255, 0, 0),
+            'green': (0, 255, 0),
+            'black': (0, 0, 0),
+            'white': (255, 255, 255),
+        }
+        self.target_color = ('red',)
+        self.size = (640,480)
+
+    def run(self, img):
+        img_copy = img.copy()
+        img_h, img_w = img.shape[:2]
+        cv2.line(img, (0, int(img_h / 2)), (img_w, int(img_h / 2)), (0, 0, 200), 1)
+        cv2.line(img, (int(img_w / 2), 0), (int(img_w / 2), img_h), (0, 0, 200), 1)
+
+        frame_resize = cv2.resize(img_copy, self.size, interpolation=cv2.INTER_NEAREST)
+        frame_gb = cv2.GaussianBlur(frame_resize, (11, 11), 11)
+        frame_lab = cv2.cvtColor(frame_gb, cv2.COLOR_BGR2LAB)  # Convert image to LAB space
+
+        areaMaxContour, area_max = self.get_contours(frame_lab)
+        if area_max > 2500:  # have found the largest area
+            rect = cv2.minAreaRect(areaMaxContour)
+            box = np.int0(cv2.boxPoints(rect))
+
+            roi = getROI(box)  # get roi region
+            get_roi = True
+
+            img_centerx, img_centery = getCenter(rect, roi, self.size,
+                                                 square_length)  # Get the coordinates of the center of the block
+            world_x, world_y = convertCoordinate(img_centerx, img_centery, self.size)  # Convert to real world coordinates
+
+            cv2.drawContours(img, [box], -1, self.range_rgb[self.target_color], 2)
+            cv2.putText(img, '(' + str(world_x) + ',' + str(world_y) + ')', (min(box[0, 0], box[2, 0]), box[2, 1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, self.range_rgb[self.target_color], 1)  # draw center point
+
+        return img
+
+    # Find the contour with the largest area
+    # argument is a list of contours to compare
+    def getAreaMaxContour(self, contours):
+        contour_area_temp = 0
+        contour_area_max = 0
+        area_max_contour = None
+
+        for c in contours:  # iterate over all contours
+            contour_area_temp = math.fabs(cv2.contourArea(c))  # Calculate the contour area
+            if contour_area_temp > contour_area_max:
+                contour_area_max = contour_area_temp
+                if contour_area_temp > 300:  # The contour with the largest area is valid only if the area is greater than 300 to filter out the noise
+                    area_max_contour = c
+
+        return area_max_contour, contour_area_max  # returns the largest contour
+
+    def get_contours(self, frame_lab):
+        area_max = 0
+        areaMaxContour = 0
+        for i in color_range:
+            if i in self.target_color:
+                detect_color = i
+                frame_mask = cv2.inRange(frame_lab, color_range[detect_color][0], color_range[detect_color][
+                    1])  # Bitwise operations on the original image and mask
+                opened = cv2.morphologyEx(frame_mask, cv2.MORPH_OPEN, np.ones((6, 6), np.uint8))  # open operation
+                closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, np.ones((6, 6), np.uint8))  # closed operation
+                contours = cv2.findContours(closed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[-2]  # find the outline
+                areaMaxContour, area_max = self.getAreaMaxContour(contours)  # find the largest contour
+
+        return areaMaxContour, area_max
+
+
+if __name__ == '__main__':
+    color_tracker = Perception()
+    my_camera = Camera.Camera()
+    my_camera.camera_open()
+    while True:
+        img = my_camera.frame
+        if img is not None:
+            frame = img.copy()
+            Frame = color_tracker.run(frame)
+            cv2.imshow('Frame', Frame)
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+    my_camera.camera_close()
+    cv2.destroyAllWindows()
+
